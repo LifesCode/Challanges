01b6c1043d47074dffc5e7316e7841d0c18d5c1b
robertocarlosmedina <robertocarlosmedina.dev@gmail.com>
2021-11-26 21:32:20 -0100

    Adding Ronaldo

--- connecting_random_dots/ronaldo_crd.py
+++ connecting_random_dots/ronaldo_crd.py
@@ -1,18 +1,16 @@
 import pygame
+import random as r
 
 pygame.init()
 
 WIDTH, HEIGHT = 700, 500
 BG_COLOR = (0, 150, 150)
 FPS = 60
+CONNECTING_DISTANCE = 80
 CLOCK = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dots challenge")
 
-
-class Fragment_point:
-    class __init__(self, screen: pygame.Surface):
-        pass
 
 def main():
     while True:
@@ -20,8 +18,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-            
-        screen.fill(BG_COLOR)
+        pygame.draw.rect(screen, BG_COLOR, (200, 200, 200, 200))
         pygame.display.update()
 
 
