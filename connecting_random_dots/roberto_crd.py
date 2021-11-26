92188e7c1354e0fc47a3a8d6c8304caf13f92271
robertocarlosmedina <robertocarlosmedina.dev@gmail.com>
2021-11-26 21:10:54 -0100

    starting

--- connecting_random_dots/roberto_crd.py
+++ connecting_random_dots/roberto_crd.py
@@ -3,6 +3,7 @@ import pygame
 pygame.init()
 
 WIDTH, HEIGHT = 700, 500
+BG_COLOR = (0, 150, 150)
 FPS = 60
 CLOCK = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -15,6 +16,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+            
+        screen.fill(BG_COLOR)
+        pygame.display.update()
 
 
 if __name__ == "__main__":
