e53fc3a26eb0a196a48bb5f8e3c6b02a504adae2
RaffaeleFiorillo <raffadeus8@gmail.com>
2021-11-26 20:00:42 -0100

    further organization of the repository

---
+++ initial_setup/README.md
@@ -0,0 +1,3 @@
+# Initial Setup
+The initial setup of every challange. It has only the basic code to create and display a window in pygame
+
---
+++ initial_setup/main.py
@@ -0,0 +1,14 @@
+import pygame
+import random as r
+
+pygame.init()
+
+WIDTH, HEIGHT = 700, 500
+CLOCK = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Dots challenge")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
