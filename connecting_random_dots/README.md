3391dcc5f76ab819f9dd5148933a4e0040874796
RaffaeleFiorillo <raffadeus8@gmail.com>
2021-12-03 20:47:52 -0100

    draws half the lines it was drawing before

--- connecting_random_dots/README.md
+++ connecting_random_dots/README.md
@@ -9,3 +9,7 @@ dots move in the screen, and the mouse (cursor) connects with nearby dots with s
 3- Dots that are close to the cursor, connect with him with more clear lines
 
 4- Dots fade when they get too far away from each other
+
+interesting:
+Make dots show some kind of identification. like a number or ID
+this would make it easier to study the movements of dots
--- connecting_random_dots/raffaele_crd.py
+++ connecting_random_dots/raffaele_crd.py
@@ -3,27 +3,29 @@ import random as r
 
 pygame.init()
 
-WIDTH, HEIGHT = 900, 720
-BG_COLOR = (0, 0, 0)  # default is (0, 150, 150)
+WIDTH, HEIGHT = 1080, 720
+BG_COLOR = (0, 0, 0)  # default for project was (0, 150, 150)
 BG = pygame.Surface((WIDTH, HEIGHT))
 BG.fill(BG_COLOR)  # set screen with background color
-FPS = 30
+FPS = 1
 MOUSE_LINE_COLOR = (0, 0, 255)
+MAX_SPEED, MIN_SPEED = 4, 0  # maximum and minimum speed that a dot can reach
 CONNECTING_DISTANCE = 200
 DOT_CLUSTER_DISTANCE = 80
-DOT_NUMBER = 200
-MAX_DOT_NUMBER = DOT_NUMBER*1.5
+DOT_NUMBER = 200  # dot number in the normal execution (static)
+MAX_DOT_NUMBER = DOT_NUMBER * 1.5  # max number of dots that can exist
 CLOCK = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dots challenge")
 
 
 class Dot:
-    def __init__(self, x=None, y=None):
-        self.position = (r.randint(0, WIDTH), r.randint(0, HEIGHT)) if x is None else (x, y)
-        self.speed = (r.randint(0, 3), r.randint(0, 3))
+    def __init__(self, position=None):
+        self.position = (r.randint(0, WIDTH), r.randint(0, HEIGHT)) if position is None else position
+        self.speed = (r.randint(MIN_SPEED, MAX_SPEED), r.randint(MIN_SPEED, MAX_SPEED))
         self.size = 2
-        self.color = (255, 255, 0)  # (r.randint(0, 255), r.randint(0, 255), r.randint(0, 255))
+        self.movement_probability = [0.95, 0.95, 0.95]  # change in module probability|x axis change p.| y ax. change p.
+        self.color = (255, 255, 0)  # (r.randint(0, 255), r.randint(0, 255), r.randint(0, 255)) -> random color dots
         self.rect = ()
         self.update_rect()
 
@@ -31,16 +33,16 @@ class Dot:
         self.rect = (self.position[0], self.position[1], self.size, self.size)
 
     def update_speed(self):
-        if r.random() > 0.95:
-            self.speed = ((self.speed[0]+r.random()*r.choice([1, -1])) % 4,
-                          (self.speed[1]+r.random()*r.choice([1, -1])) % 4)
-        if r.random() > 0.95:
-            self.speed = (self.speed[0]*-1, self.speed[1])
-        if r.random() > 0.95:
-            self.speed = (self.speed[0], self.speed[1]*-1)
+        if r.random() > self.movement_probability[0]:  # change the module
+            self.speed = ((self.speed[0] + r.random() * r.choice([1, -1])) % MAX_SPEED,
+                          (self.speed[1] + r.random() * r.choice([1, -1])) % MAX_SPEED)
+        if r.random() > self.movement_probability[1]:  # change the x axis direction
+            self.speed = (self.speed[0] * -1, self.speed[1])
+        if r.random() > self.movement_probability[2]:  # change the y axis direction
+            self.speed = (self.speed[0], self.speed[1] * -1)
 
     def update(self):
-        self.position = ((self.position[0]+self.speed[0]) % WIDTH, (self.position[1]+self.speed[1]) % HEIGHT)
+        self.position = ((self.position[0] + self.speed[0]) % WIDTH, (self.position[1] + self.speed[1]) % HEIGHT)
         self.update_rect()
         self.update_speed()
 
@@ -54,19 +56,19 @@ class Dot:
 
 def refresh(screen_in, dots):
     screen_in.blit(BG, (0, 0))
-    [dot.draw(screen_in) for dot in dots]
     draw_straight_lines(screen, dots)
+    [dot.draw(screen_in) for dot in dots]
     pygame.display.update()
 
 
 def draw_straight_lines(screen_in, dots):  # draws the lines between objects (dot2dot and dot2cursor)
     mouse_position = pygame.mouse.get_pos()
-    for dot_main in dots:
-        for dot_in in dots:
+    for main_dot_index, dot_main in enumerate(dots):
+        for dot_in in dots[main_dot_index+1:]:
             vector = dot_main.vector_distance(dot_in.position)  # distance between two dots
-            if dot_main is not dot_in and vector <= DOT_CLUSTER_DISTANCE:
+            if vector <= DOT_CLUSTER_DISTANCE:
                 # default was (255*vector//DOT_CLUSTER_DISTANCE) if vector > DOT_CLUSTER_DISTANCE*0.6 else 150
-                color = 255-(255*vector//DOT_CLUSTER_DISTANCE) if vector > DOT_CLUSTER_DISTANCE*0.6 else 100  # fading
+                color = 255 - (255 * vector // DOT_CLUSTER_DISTANCE) if vector > DOT_CLUSTER_DISTANCE * 0.6 else 100
                 pygame.draw.line(screen_in, (0, color, color), dot_main.position, dot_in.position, 1)
         if dot_main.vector_distance(mouse_position) <= CONNECTING_DISTANCE:
             pygame.draw.line(screen_in, MOUSE_LINE_COLOR, mouse_position, dot_main.position, 1)
@@ -79,9 +81,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-            elif pygame.mouse.get_pressed(3)[0] and len(dots) < MAX_DOT_NUMBER:
-                x, y = pygame.mouse.get_pos()
-                dots.append(Dot(x, y))
+            elif len(dots) < MAX_DOT_NUMBER and pygame.mouse.get_pressed(3)[0]:
+                dots.append(Dot(pygame.mouse.get_pos()))
         refresh(screen, dots)
 
 
