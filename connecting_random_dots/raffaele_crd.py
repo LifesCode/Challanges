05744fd670c1d4f3566feebdae8f2bd89d47f611
RaffaeleFiorillo <raffadeus8@gmail.com>
2021-12-06 11:39:16 -0100

    Drastic improvement in speed performance

--- connecting_random_dots/raffaele_crd.py
+++ connecting_random_dots/raffaele_crd.py
@@ -7,11 +7,11 @@ WIDTH, HEIGHT = 1080, 720
 BG_COLOR = (0, 0, 0)  # default for project was (0, 150, 150)
 BG = pygame.Surface((WIDTH, HEIGHT))
 BG.fill(BG_COLOR)  # set screen with background color
-FPS = 1
+FPS = 30
 MOUSE_LINE_COLOR = (0, 0, 255)
 MAX_SPEED, MIN_SPEED = 4, 0  # maximum and minimum speed that a dot can reach
-CONNECTING_DISTANCE = 200
-DOT_CLUSTER_DISTANCE = 80
+CONNECTING_DISTANCE = 300
+DOT_CLUSTER_DISTANCE = 120
 DOT_NUMBER = 200  # dot number in the normal execution (static)
 MAX_DOT_NUMBER = DOT_NUMBER * 1.5  # max number of dots that can exist
 CLOCK = pygame.time.Clock()
@@ -47,22 +47,22 @@ class Dot:
         self.update_speed()
 
     def vector_distance(self, p2):
-        return ((self.position[0] - p2[0]) ** 2 + (self.position[1] - p2[1]) ** 2) ** 0.5
+        return abs(self.position[0] - p2[0]) + abs(self.position[1] - p2[1])
 
     def draw(self, screen_in):
         pygame.draw.rect(screen_in, self.color, self.rect, self.size)
         self.update()
 
 
-def refresh(screen_in, dots):
+def refresh(screen_in, dots, mouse_motion):
     screen_in.blit(BG, (0, 0))
-    draw_straight_lines(screen, dots)
+    draw_straight_lines(screen, dots, mouse_motion)
     [dot.draw(screen_in) for dot in dots]
     pygame.display.update()
 
 
-def draw_straight_lines(screen_in, dots):  # draws the lines between objects (dot2dot and dot2cursor)
-    mouse_position = pygame.mouse.get_pos()
+def draw_straight_lines(screen_in, dots, mouse_motion):  # draws the lines between objects (dot2dot and dot2cursor)
+    mouse_position = pygame.mouse.get_pos() if mouse_motion else (-100, -100, -100)  # "else" just avoids syntax error
     for main_dot_index, dot_main in enumerate(dots):
         for dot_in in dots[main_dot_index+1:]:
             vector = dot_main.vector_distance(dot_in.position)  # distance between two dots
@@ -70,12 +70,13 @@ def draw_straight_lines(screen_in, dots):  # draws the lines between objects (do
                 # default was (255*vector//DOT_CLUSTER_DISTANCE) if vector > DOT_CLUSTER_DISTANCE*0.6 else 150
                 color = 255 - (255 * vector // DOT_CLUSTER_DISTANCE) if vector > DOT_CLUSTER_DISTANCE * 0.6 else 100
                 pygame.draw.line(screen_in, (0, color, color), dot_main.position, dot_in.position, 1)
-        if dot_main.vector_distance(mouse_position) <= CONNECTING_DISTANCE:
+        if mouse_motion and dot_main.vector_distance(mouse_position) <= CONNECTING_DISTANCE:
             pygame.draw.line(screen_in, MOUSE_LINE_COLOR, mouse_position, dot_main.position, 1)
 
 
 def main():
     dots = [Dot() for _ in range(DOT_NUMBER)]
+    mouse_motion = False
     while True:
         CLOCK.tick(FPS)
         for event in pygame.event.get():
@@ -83,7 +84,8 @@ def main():
                 exit()
             elif len(dots) < MAX_DOT_NUMBER and pygame.mouse.get_pressed(3)[0]:
                 dots.append(Dot(pygame.mouse.get_pos()))
-        refresh(screen, dots)
+            mouse_motion = event.type == pygame.MOUSEMOTION  # True if mouse has moved, False otherwise
+        refresh(screen, dots, mouse_motion)
 
 
 if __name__ == "__main__":
